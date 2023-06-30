79019c81ca7a12f791e9a1d9f96663d58faefbb6
lucifer1708 <20bce091@nith.ac.in>
2023-06-30 14:31:30 +0530

    fixed login

--- bank/authentication.py
+++ bank/authentication.py
@@ -1,18 +1,20 @@
-from django.contrib.auth.backends import ModelBackend
-from .models import User
+# from django.contrib.auth.backends import ModelBackend
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = User.objects.get(email=username)
-        except User.DoesNotExist:
-            return None
-        else:
-            if user.check_password(password):
-                return user
+# from .models import User
 
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+
+# class EmailBackend(ModelBackend):
+#     def authenticate(self, request, username=None, password=None, **kwargs):
+#         try:
+#             user = User.objects.get(email=username)
+#         except User.DoesNotExist:
+#             return None
+#         else:
+#             if user.check_password(password):
+#                 return user
+
+#     def get_user(self, user_id):
+#         try:
+#             return User.objects.get(pk=user_id)
+#         except User.DoesNotExist:
+#             return None
--- bank/views.py
+++ bank/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout, get_user_model
-from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import LoginForm, SignUpForm
 
 
 # Create your views here.
@@ -10,39 +11,39 @@ def index(request):
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect("login")
     else:
         form = SignUpForm()
-    return render(request, 'bank/signup.html', {'form': form})
+    return render(request, "bank/signup.html", {"form": form})
 
 
-def login(request):
-    if request.method == 'POST':
+def login_view(request):
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
             User = get_user_model()
             user = User.objects.filter(username=email).first()
             if user is not None and user.check_password(password):
                 login(request, user)
-                return redirect('dashboard')
+                return redirect("dashboard")
             else:
-                form.add_error(None, 'Invalid username or password.')
+                form.add_error(None, "Invalid username or password.")
     else:
         form = LoginForm()
-    return render(request, 'bank/login.html', {'form': form})
+    return render(request, "bank/login.html", {"form": form})
 
 
-def logout(request):
+def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, "dashboard.html")
